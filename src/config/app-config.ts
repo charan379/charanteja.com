c7d95983437dfeea64967e35e0f7672e6cf7bf24
Charan Teja Yandrapati <37008444+charan379@users.noreply.github.com>
2026-09-15 08:21:00 +0000

    Fix typo in 'src' property for image objects

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -649,13 +649,13 @@ export const appConfig: AppConfig = {
     {
       id: "space-stars",
       type: "image",
-      scr: "https://images.unsplash.com/photo-1464802686167-b939a6910659?q=80&w=2564&auto=format&fit=crop",
+      src: "https://images.unsplash.com/photo-1464802686167-b939a6910659?q=80&w=2564&auto=format&fit=crop",
       alt: "space stars",
     },
     {
       id: "space-orange-stars",
       type: "image",
-      scr: "https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?q=80&w=2564&auto=format&fit=crop",
+      src: "https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?q=80&w=2564&auto=format&fit=crop",
       alt: "space  orange stars",
     }
   ],
